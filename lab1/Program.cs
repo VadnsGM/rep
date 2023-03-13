be627fac1816e04a42de5ab5508f513b6d01d6fa
1234567890 <jdanovich2@gmail.com>
2023-03-13 17:12:52 +0300

    saved

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -2,6 +2,12 @@
 {
     internal class program
     {
+        public class river
+        {
+            public string name { get; set; }
+            public double square { get; set; }
+            public double length { get; set; }
+        }
         static void Main(string[] args)
         {
 
